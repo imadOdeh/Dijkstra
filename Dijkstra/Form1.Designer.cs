407007556f1a6fb26f242dcb66d4a4abbb4b9a6f
Imad Odeh <en.imadodeh@gmail.com>
2022-12-20 21:35:06 +0300

    First commit

---
+++ Dijkstra/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Dijkstra
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(633, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import Excel File";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(106, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(546, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Finding Shortest Path using Dijkstra with Priority Queue";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Location = new System.Drawing.Point(404, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(100, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 28);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 593);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dijkstra with Priority Queue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+    }
+}
---
+++ Dijkstra/Form1.cs
@@ -0,0 +1,31 @@
+﻿namespace Dijkstra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var adj = new List<List<Dijkstra.Node>>()
+            {
+                new List<Dijkstra.Node> { new Dijkstra.Node("İzmit", 1, 200), new Dijkstra.Node("Tekirdağ", 2, 150) },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Sakarya", 3, 250), new Dijkstra.Node("Bursa", 4, 300) },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Edirne", 5, 100) },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Bolu", 6, 100), new Dijkstra.Node("Bilecek", 7, 300) },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Bilecek", 7, 100) },
+                new List<Dijkstra.Node> { },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Ankara", 8, 100) },
+                new List<Dijkstra.Node> { new Dijkstra.Node("Ankara", 8, 100) },
+                new List<Dijkstra.Node> { },
+            };
+
+            var dijkstra = new Dijkstra(9, new Dijkstra.Node("Istanbul", 0, 0), adj);
+            var result = dijkstra.ExeucteAlgorithm();
+            
+            label2.Text = string.Join(',', result);
+        }
+    }
+}
